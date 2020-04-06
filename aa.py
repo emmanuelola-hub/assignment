d47cb053066a8da49992bdb63fe378881aebde14
Olayiwola <olayiwolaemma49@gmail.com>
2020-04-06 11:15:34 -0700

    assignment

---
+++ aa.py
@@ -0,0 +1,27 @@
+
+
+
+
+pin = "123456"
+
+print("please slot in your card")
+
+
+var = input("pin ")
+while pin != var:
+        print("wrong try again")
+        var = input("please put in the correct pin ")
+print("savings account or current account")
+var_1 = input("choose your account?")
+if var_1 == "savings or current account":
+        print("how much do you want to withdraw? #")
+else:
+        print("wrong try again")
+var_2 = input("how much do you want to withdraw? #")
+for x in range(200,000):
+    print(x)
+    if x == 10000:
+        break
+    print(x)
+
+print("thank you for choosing this bank!")
---
+++ atm.py
@@ -0,0 +1,18 @@
+
+
+
+
+pin = "123456"
+
+print("please slot in your card")
+
+
+var = input("pin ")
+while pin != var:
+        print("wrong try again")
+        var = input("please put in the correct pin ")
+print("savings account or current account")
+var_1 = input("choose your account?")
+var_2 = input("how much do you want to withdraw? #")
+print("thank you for choosing this bank!")
+
